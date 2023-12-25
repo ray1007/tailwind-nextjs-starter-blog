4d2b22112e922e8ae2a694308dd88f5184db7b0e
ray1007 <raysu3329@gmail.com>
2023-12-25 16:17:42 -0500

    fix bug

--- components/article/AmazonProductList.js
+++ components/article/AmazonProductList.js
@@ -1,26 +1,34 @@
 'use client'
 
-import { useInView } from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer'
 
 export default function Table({ products }) {
   const { ref, inView } = useInView({
     threshold: 0,
-    triggerOnce: true
-  });
+    triggerOnce: true,
+  })
 
   return (
-    <div className="flex gap-4 overflow-x-auto pb-2 my-2 md:pb-0" style={{ height: '240px' }} ref={ref}>
-      {inView && products && products.map(({productSrc}) => 
-        <iframe
-          sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-          style={{ width: '120px', height: '240px' }}
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameBorder="0"
-          src={productSrc}
-        />
-      )}
+    <div
+      className="my-2 flex gap-4 overflow-x-auto pb-2 md:pb-0"
+      style={{ height: '240px' }}
+      ref={ref}
+    >
+      {inView &&
+        products &&
+        products.map(({ productSrc }, idx) => (
+          <iframe
+            key={idx}
+            title={`amazon product ${idx}`}
+            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+            style={{ width: '120px', height: '240px' }}
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameBorder="0"
+            src={productSrc}
+          />
+        ))}
     </div>
   )
 }
