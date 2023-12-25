62f9299f5dcb589b49b1129d0e3ff9784706e439
ray1007 <raysu3329@gmail.com>
2023-12-25 11:29:17 -0500

    fix kbar bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,15 +76,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
-          <Header />
-          <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+          <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+            <Header />
+            <SectionContainer>
+              <div className="flex h-screen flex-col justify-between font-sans">
                 <main className="mb-auto">{children}</main>
-              </SearchProvider>
-              <Footer />
-            </div>
-          </SectionContainer>
+                <Footer />
+              </div>
+            </SectionContainer>
+          </SearchProvider>
         </ThemeProviders>
       </body>
     </html>
