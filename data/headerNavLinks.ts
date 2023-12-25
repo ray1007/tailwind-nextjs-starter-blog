6a3813d903f6d15238b5104becfcbb78dc9b5104
ray1007 <raysu3329@gmail.com>
2023-12-25 12:08:18 -0500

    remove unused file

--- data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blogs', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
