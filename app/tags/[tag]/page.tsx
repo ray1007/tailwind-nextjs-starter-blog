96c3304e793c22fc5c81b7149d8e88d6da03bcfe
ray1007 <raysu3329@gmail.com>
2023-12-25 16:30:30 -0500

    fix tag page

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -8,10 +8,10 @@ import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
 
 export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
-  const tag = decodeURI(params.tag)
+  const tag = decodeURI(decodeURI(params.tag))
   return genPageMetadata({
     title: tag,
-    description: `${siteMetadata.title} ${tag} tagged content`,
+    description: `${siteMetadata.title}：含有 ${tag} 標籤的文章`,
     alternates: {
       canonical: './',
       types: {
@@ -26,12 +26,13 @@ export const generateStaticParams = async () => {
   const tagKeys = Object.keys(tagCounts)
   const paths = tagKeys.map((tag) => ({
     tag: encodeURI(tag),
+    //tag: encodeURI(tag),
   }))
   return paths
 }
 
 export default function TagPage({ params }: { params: { tag: string } }) {
-  const tag = decodeURI(params.tag)
+  const tag = decodeURI(decodeURI(params.tag))
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
